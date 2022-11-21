b3f22f0c54fe8ca8a161d3ac5cbf348d015316b8
Julian <jll38@njit.edu>
2022-11-21 18:49:18 -0500

    text input, formatting, API

--- src/components/weather.js
+++ src/components/weather.js
@@ -1,6 +1,9 @@
 import React, {useState} from "react";
+import { config } from "../index";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-
+//Weather Data as component from Location String
 export default class Weather extends React.Component{
     constructor(props){
         super(props);
@@ -12,15 +15,28 @@ export default class Weather extends React.Component{
         return(
             <>
                 <h1>Weather</h1>
-                <h2>{process.env.REACT_APP_WEATHER_API_KEY}</h2>
-                {handleLocation('New York, NY')}
+                <Form>
+                    <Form.Group className="mb-2 w-50" controlId="formWeatherLocation">
+                      <Form.Label>Location</Form.Label>
+                        <Form.Control type="text" placeholder="New York, NY" />
+                     </Form.Group>
+                </Form>
+                {getWeather('London')}
             </>
         )
     }
 }
 
-//Pass weather object from Fetch API call
-function handleLocation(props){
+
+async function handleLocation(props){
+//    const weatherRes = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${props}&limit=5&appid=${config.WeatherAPIKey}`)
+//    .then((response) => response.json())
+//    .then((data) =>console.log(data))
+}
+
+
+function getWeather(props){
+    handleLocation(props)
     return(
         <>
         <h2>{props}</h2>
